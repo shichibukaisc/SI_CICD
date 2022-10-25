33de734dfcddd19545b148c20cace9e0da1a252a
cloud <cloud@shinra.com>
2022-10-24 21:00:20 -0600

    Increase JDK 17.0.5

--- src/test/java/ca/alberta/services/sithdfca/ContrivedTest.java
+++ src/test/java/ca/alberta/services/sithdfca/ContrivedTest.java
@@ -9,7 +9,7 @@ public class ContrivedTest {
 	@Test
 	public void checkJavaVersion() {
 		String currentVersion = System.getProperty("java.version");
-		boolean tooModern = currentVersion.compareTo("17.0.1") > 0;
+		boolean tooModern = currentVersion.compareTo("17.0.5") > 0;
 		assertFalse(tooModern, String.format("java version %s is too modern!", currentVersion));
 	}
 }
